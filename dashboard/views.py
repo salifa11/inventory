0bd6cf1f6a9c344467ea8b941be586a0d6a09bf5
salifa11 <salifashrestha5000@gmail.com>
2025-02-05 15:36:11 +0545

    Initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request,'dashboard/index.html')
+    
+def category(request):
+   return render(request,'dashboard/category.html')
+    
